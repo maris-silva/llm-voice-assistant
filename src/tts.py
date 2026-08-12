0bc3904622e291963376abcfbf824bd466859e27
Leonardo Dias do Carmo <leonardo_carmo@usp.br>
2026-08-12 13:52:58 -0300

    add tts example

---
+++ src/tts.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def falar(texto, voz="pt-br"):
+    espeak = subprocess.Popen(
+        ["espeak-ng", "-v", voz, "--stdout", texto],
+        stdout=subprocess.PIPE
+    )
+    subprocess.run(
+        ["aplay", "-D", "default"],
+        stdin=espeak.stdout
+    )
+    espeak.stdout.close()
+
+if __name__ == "__main__":
+    falar("Olá, isso é um teste de texto para voz rodando em Python no Raspberry Pi")
